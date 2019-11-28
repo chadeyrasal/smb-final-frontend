9e33dd87fc800384de28c7087159d79be347b283
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-28 15:51:09 +0000

    import packages to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'; // we need access to the createStore method to be able to set up a store
+import thunk from 'redux-thunk'; // for async redux
+import { Provider } from 'react-redux'; // any component we wrap in the provider will have access to the Redux store
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
