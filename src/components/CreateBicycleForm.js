edf14592d7104e7e0f768309c8c0c2ce0002846c
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-29 16:23:22 +0000

    create initial setup for Create Bicycle Form class component

--- src/components/CreateBicycleForm.js
+++ src/components/CreateBicycleForm.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class CreateBicycleForm extends React.Component {
+  render() {
+    return (
+      <div>
+        New Bicycle Form
+      </div>
+    )
+  }
+}
+
+export default CreateBicycleForm;
