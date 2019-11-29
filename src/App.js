001629ebe792c8cd4b6164997f318d227350618b
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-29 15:11:56 +0000

    modify the list of imports to have access to all containers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {fetchCities} from './actions/fetchCities';
+import CitiesContainer from './containers/CitiesContainer';
+import BicyclesContainer from './containers/BicyclesContainer';
+import CountriesContainer from './containers/CountriesContainer';
+import NeighbourhoodsContainer from './containers/NeighbourhoodsContainer';
+import TripsContainer from './containers/TripsContainer';
+import UsersContainer from './containers/UsersContainer';
 
 class App extends React.Component {
   componentDidMount() {
