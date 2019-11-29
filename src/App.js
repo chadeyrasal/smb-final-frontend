aca94869fda92c96c289ac04762eaee00b8a2680
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-29 10:54:25 +0000

    set up connect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchCities} from './actions/fetchCities';
 
 class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchCities({type: 'FETCH_CITIES', payload: {name: "Paris"}})
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,4 +16,14 @@ class App extends React.Component {
   }
 }
 
-export default App;
+// allows access to current store values as props
+// const mapStateToProps => (state) {
+//   return {
+//     cities: state.cities
+//   }
+// }
+
+// 1 - by setting a second argument to connect, we can then dispatch new actions to the store from this component
+// 2 - if we just need to dispatch actions to the store - not getting current store values - we just need to pass
+// null as first argument
+export default connect(null, {fetchCities})(App);
--- src/actions/fetchCities.js
+++ src/actions/fetchCities.js
@@ -1,3 +1,8 @@
-export function fetchCities() {
-
+export function fetchCities(action) {
+  // fetch('http://localhost:3000/api/v1/cities')
+  //   .then(response => response.json())
+  //   .then(cities => {
+  //     console.log(cities)
+  //   })
+  return action
 }
