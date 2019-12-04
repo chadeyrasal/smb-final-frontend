5400a04a25273600fd6e35eafbb5b86d47357978
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-04 16:33:27 +0000

    build first version of new bicycle form

--- src/components/CreateBicycleForm.js
+++ src/components/CreateBicycleForm.js
@@ -4,7 +4,67 @@ class CreateBicycleForm extends React.Component {
   render() {
     return (
       <div>
-        New Bicycle Form
+        <form>
+          <h2>New Listing</h2><br/>
+          <label>Listing Title: </label><br/>
+          <input type='text'></input><br/><br/>
+          <label>Listing Description: </label><br/>
+          <input type='text'></input><br/><br/>
+          <label>Bicycle Type: </label><br/>
+            <div className="radio">
+              <label>
+                <input type="radio" value="option1" />
+                City Bicycle
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input type="radio" value="option2" />
+                Mountain Bicycle
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input type="radio" value="option3" />
+                Road Bicycle
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input type="radio" value="option4" />
+                Folding Bicycle
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input type="radio" value="option5" />
+                Trekking Bicycle
+              </label>
+            </div><br/>
+          <label>Bicycle Size: </label>
+            <div className="radio">
+              <label>
+                <input type="radio" value="option6" />
+                Small
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input type="radio" value="option7" />
+                Medium
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input type="radio" value="option8" />
+                Large
+              </label>
+            </div><br/>
+          <label>Bicycle Colour: </label><br/>
+          <input type='text'></input><br/><br/>
+          <label>Price: </label><br/>
+          <input type='text'></input><br/><br/>
+        </form>
       </div>
     )
   }
