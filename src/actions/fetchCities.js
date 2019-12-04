f7ca8171484985581314f3965022fbbbdfaab768
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-04 14:21:11 +0000

    add dispatch to fetchCities action to give theaction creator a type and a payload

--- src/actions/fetchCities.js
+++ src/actions/fetchCities.js
@@ -1,7 +1,16 @@
 export function fetchCities() {
-  fetch('http://localhost:3000/api/v1/cities')
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/cities')
     .then(response => response.json())
     .then(cities => {
-      console.log(cities)
+      dispatch({
+        type: 'FETCH_CITIES',
+        payload: cities
+      })
     })
+  }
 }
+
+// this is a thunk function that has dipatch as argument so that we can use it inside our action creator
+// this way we dispatch only when the fetch request is complete
+// dispatch() must have ak ey of type, and here since we are queriing our backend we also need payload
