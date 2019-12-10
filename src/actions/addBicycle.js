78643224447ecd59a1f8bb235b194a54752d1b19
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-10 16:11:04 +0000

    build basic fetch request with url and additional settings for post request

--- src/actions/addBicycle.js
+++ src/actions/addBicycle.js
@@ -1,3 +1,12 @@
 export const addBicycle = (formData) => {
-  return (dispatch)
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/bicycles', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(formData) // this way formData is treatedas a string, when initially it is an object
+    })
+  }
 }
