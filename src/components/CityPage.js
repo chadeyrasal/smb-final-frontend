62e57a771923aab970dc29f07711f9119e504bd8
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-11 13:31:46 +0000

    format city page

--- src/components/CityPage.js
+++ src/components/CityPage.js
@@ -6,10 +6,10 @@ const CityPage = (props) => {
   // Won't work if some entries have been deleted in the backend, because then id - 1 won't match array index
 
   return (
-    <li>
-      {city ? null : <Redirect to='/cities'/>}
-      {city ? city.name : null} - {city ? city.country.name : null}
-    </li>
+    <div>
+      <h2>{city ? city.name : null}</h2>
+      <h3>{city ? city.country.name : null}</h3>
+    </div>
   )
 }
 
