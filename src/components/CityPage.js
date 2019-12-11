651aabe5614670c437377d085c89f304c699e513
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-11 11:39:29 +0000

    add redirect to city page

--- src/components/CityPage.js
+++ src/components/CityPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 
 const CityPage = (props) => {
   let city = props.cities[props.match.params.id - 1]
+  // Won't work if some entries have been deleted in the backend, because then id - 1 won't match array index
 
   return (
     <li>
+      {city ? null : <Redirect to='/cities'/>}
       {city ? city.name : null} - {city ? city.country.name : null}
     </li>
   )
