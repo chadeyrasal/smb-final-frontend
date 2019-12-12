d0c5756bcc339deaa65a5220a69d9bf70ff6564c
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-12 19:14:50 +0000

    changesyntax to ES6

--- src/actions/fetchCityBicycles.js
+++ src/actions/fetchCityBicycles.js
@@ -1,4 +1,4 @@
-export function fetchCityBicycles() {
+export const fetchCityBicycles = () => {
   fetch('http://localhost:3000/api/v1/cities/1/bicycles')
     .then(response => response.json())
     .then(cityBicycles => console.log(cityBicycles))
