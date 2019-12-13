0e8287084952699e66325e8a45c0e4008561783f
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-13 17:20:39 +0000

    add new switch case fr reset login form

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -7,6 +7,8 @@ export default function loginReducer(state = initialSate, action) {
   switch (action.type) {
     case 'UPDATE_LOGIN_FORM':
       return action.payload
+    case 'RESET_LOGIN_FORM':
+      return initialSate
     default:
       return state
   }
