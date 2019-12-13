8640b85ab9d6538ec55427ddaf8bae562fd29713
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-13 15:47:19 +0000

    add CLEAR CURRENT USER case to witch statement

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -1,7 +1,9 @@
-export default function currentUserReducer(state = {}, action) {
+export default function currentUserReducer(state = null, action) {
   switch (action.type) {
     case 'SET_CURRENT_USER':
       return action.payload
+    case 'CLEAR_CURRENT_USER':
+      return null
     default:
       return state
   }
