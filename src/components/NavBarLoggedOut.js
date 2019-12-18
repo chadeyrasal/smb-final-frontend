60e83316e5558eaa93bdc87b825fca35a765fecf
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-18 17:29:27 +0000

    setup navbar when logged out with semantic ui

--- src/components/NavBarLoggedOut.js
+++ src/components/NavBarLoggedOut.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Input, Menu, Segment } from 'semantic-ui-react';
 
-const NavBarLoggedOut = () => {
-  return(
-    <div>
-      Nav Bar when logged out
-    </div>
-  )
+class NavBarLoggedOut extends React.Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (event, { name }) => {
+    this.setState({ activeItem: name })
+    {/*send to appropriate route according to name*/}
+  }
+
+  render() {
+    const activeItem = this.state
+
+    return(
+      <Segment>
+        <Menu secondary>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}>Home</Menu.Item>
+          <Menu.Item>
+            <Input icon='search' placeholder='Search cities...' />
+          </Menu.Item>
+          <Menu.Menu position='right'>
+            <Menu.Item name='log in' active={activeItem === 'log in'} onClick={this.handleItemClick} />
+            <Menu.Item name='sign up' active={activeItem === 'sign up'} onClick={this.handleItemClick} />
+          </Menu.Menu>
+        </Menu>
+      </Segment>
+    )
+  }
 }
 
 export default NavBarLoggedOut;
