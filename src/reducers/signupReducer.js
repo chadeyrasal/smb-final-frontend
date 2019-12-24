98e90b77fa1dbd3373ca1a5918c5fb0075cd5935
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-24 17:52:28 +0100

    build basic signup reducer with initial state and switch statement

---
+++ src/reducers/signupReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  fullname: '',
+  email: '',
+  password: ''
+}
+
+export default function signupReducer(state = initialSate, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
