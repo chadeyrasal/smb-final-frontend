3f63c90297a3aa5413b46abf8bc7ca292b2b5135
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-26 12:55:50 +0100

    build footer component for when user is logged in

---
+++ src/components/FooterLoggedIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class FooterLoggedIn extends React.Component {
+  render() {
+    return (
+      <div>FooterLoggedIn</div>
+    )
+  }
+}
+
+export default FooterLoggedIn;
