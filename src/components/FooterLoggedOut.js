db1a43202445dd78c92575342c86a25f89452d0c
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-26 12:56:16 +0100

    build footer component for when user is logged out

---
+++ src/components/FooterLoggedOut.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class FooterLoggedOut extends React.Component {
+  render() {
+    return (
+      <div>FooterLoggedOut</div>
+    )
+  }
+}
+
+export default FooterLoggedOut;
