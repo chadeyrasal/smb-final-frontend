536962226b877e61aa9072afeac5b70a14a4402b
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-28 12:00:48 +0100

    rename world map into map chart

---
+++ src/components/MapChart.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+} from 'react-simple-maps';
+
+const geoUrl = 'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json'
+
+const MapChart = () => {
+  return (
+    <ComposableMap>
+      <Geographies geography={geoUrl}>
+        {({geographies}) => geographies.map(geo =>
+          <Geography key={geo.rsmKey} geography={geo} />
+        )}
+      </Geographies>
+    </ComposableMap>
+  );
+};
+
+export default MapChart;
