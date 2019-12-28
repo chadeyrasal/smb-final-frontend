9462f330c7ab953cbaf17e31f2dd878b7fe2d3fe
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-28 13:00:58 +0100

    display world map without antarctica and change colors of the map

--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import {
-  ComposableMap,
-  Geographies,
-  Geography,
-} from 'react-simple-maps';
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
 
 const geoUrl = 'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json'
 
 const MapChart = () => {
   return (
-    <ComposableMap>
+    <ComposableMap projection='geoMercator' projectionConfig={{scale: 100}}>
       <Geographies geography={geoUrl}>
-        {({geographies}) => geographies.map(geo =>
-          <Geography key={geo.rsmKey} geography={geo} />
-        )}
+        {({ geographies }) =>
+          geographies
+            .filter(d => d.properties.REGION_UN !== 'Antarctica')
+            .map(geo => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill="#EAEAEC"
+                stroke="#D6D6DA"
+              />
+          ))
+        }
       </Geographies>
     </ComposableMap>
   );
