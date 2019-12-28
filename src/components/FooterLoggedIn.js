d1e9a9470271248bb162d60889c383d17a107480
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-28 10:54:44 +0100

    add info on logged in user to the footer when use is logged in

--- src/components/FooterLoggedIn.js
+++ src/components/FooterLoggedIn.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Segment } from 'semantic-ui-react';
 
 class FooterLoggedIn extends React.Component {
   render() {
     return (
-      <div>FooterLoggedIn</div>
+      <div>
+        <Segment attached='bottom' secondary>
+          <h5>You are logged in as ...</h5>
+        </Segment>
+      </div>
     )
   }
 }
