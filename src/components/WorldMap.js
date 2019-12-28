db490d535c23461aba5524b8f20808744993f032
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-28 11:34:11 +0100

    build basic version of WorldMap using simple map react

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -1,2 +1,46 @@
 import React from 'react';
 import { ComposableMap, Geographies, Geography, Marker } from 'react-simple-maps';
+
+const geoUrl = 'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
+
+const markers = [];
+
+const WorldMap = () => {
+  return (
+    <ComposableMap
+      projection="geoAzimuthalEqualArea"
+      projectionConfig={{
+        rotate: [58.0, 20.0],
+        scale: 400
+      }}
+    >
+
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map(geo => (
+            <Geography
+              key={geo.rsmKey}
+              geography={geo}
+              fill="#EAEAEC"
+              stroke="#D6D6DA"
+            />
+          ))
+        }
+      </Geographies>
+
+      {markers.map(({ name, coordinates, markerOffset }) => (
+        <Marker key={name} coordinates={coordinates}>
+          <circle r={10} fill="#f00" stroke="#fff" strokeWidth={2} />
+          <text
+            textAnchor="middle"
+            y={markerOffset}
+            style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+          >
+            {name}
+          </text>
+        </Marker>
+      ))}
+
+    </ComposableMap>
+  )
+}
