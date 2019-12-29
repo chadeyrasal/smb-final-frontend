3e7d50288fcbc852eee1b8a246b7715abfb962f8
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-29 19:26:55 +0100

    setup logged out footer, same styling as logged in footer

--- src/components/FooterLoggedOut.js
+++ src/components/FooterLoggedOut.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Segment } from 'semantic-ui-react';
 
 class FooterLoggedOut extends React.Component {
   render() {
     return (
-      <div>FooterLoggedOut</div>
+      <div>
+        <Segment attached='bottom' textAlign={'center'} secondary>
+          <p><Link className='footer-para' to='/about'>About Share My Bike</Link></p>
+          <p>{'\u00a9'} Share My Bike 2019. All rights reserved</p>
+        </Segment>
+      </div>
     )
   }
 }
