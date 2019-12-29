d444a4dc1b5fd9e67209724ee1912d4a9526a1d7
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-29 18:57:32 +0100

    add logout button to logged in footer

--- src/components/FooterLoggedIn.js
+++ src/components/FooterLoggedIn.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Button } from 'semantic-ui-react';
 
 const FooterLoggedIn = ({currentUser}) => {
   return (
     <div>
-      <Segment attached='bottom' secondary>
+      <Segment attached='bottom' textAlign={'center'} secondary>
         <h5>You are logged in as <Link to={`/users/${currentUser.id}`}>{currentUser.fullname}</Link></h5>
+        <Button size='mini' basic color='black' style={{textAlign: 'center'}}>Log Out</Button>
+        
       </Segment>
     </div>
   )
