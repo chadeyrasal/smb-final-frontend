b024ee5d37fadf93c25658c760c83224be143264
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-29 19:16:26 +0100

    replacelog out button by log out component

--- src/components/FooterLoggedIn.js
+++ src/components/FooterLoggedIn.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Segment, Button, Divider } from 'semantic-ui-react';
+import LogOut from './Logout';
+import { Segment, Divider } from 'semantic-ui-react';
 
 const FooterLoggedIn = ({currentUser}) => {
   return (
     <div>
       <Segment attached='bottom' textAlign={'center'} secondary>
-        <h5>You are logged in as <Link to={`/users/${currentUser.id}`}>{currentUser.fullname}</Link></h5>
-        <Button size='mini' basic color='black' style={{textAlign: 'center'}}>Log Out</Button>
+        <h5>You are logged in as <Link to={`/users/${currentUser.id}`}>{currentUser.fullname}</Link> {'\u00b7'} <LogOut /></h5>
         <Divider />
         <p><Link className='footer-para' to='/about'>About Share My Bike</Link></p>
         <p>{'\u00a9'} Share My Bike 2019. All rights reserved</p>
