10e949b18bb71b1360ef36a320a26791d862764a
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-29 18:40:39 +0100

    make fullname of logged in user a link to the user's account page

--- src/components/FooterLoggedIn.js
+++ src/components/FooterLoggedIn.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { Segment } from 'semantic-ui-react';
 
 const FooterLoggedIn = ({currentUser}) => {
   return (
     <div>
       <Segment attached='bottom' secondary>
-        <h5>You are logged in as {currentUser.fullname}</h5>
+        <h5>You are logged in as <Link to={`/users/${currentUser.id}`}>{currentUser.fullname}</Link></h5>
       </Segment>
     </div>
   )
