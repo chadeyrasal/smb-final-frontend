04fa977dbd01a8768c31255e03297b14a9cc09ac
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-30 16:45:20 +0000

    build content and styling for city page

--- src/components/CityPage.js
+++ src/components/CityPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import NeighbourhoodsContainer from '../containers/NeighbourhoodsContainer';
 import { Link } from 'react-router-dom';
-import { Container, Card, Image, Segment, Button } from 'semantic-ui-react';
+import { Container, Card, Image, Segment, Button, List, Label, Icon } from 'semantic-ui-react';
 
 const CityPage = (props) => {
   let city = props.cities.filter(city => city.id === parseInt(props.match.params.id))[0]
 
   if (city) {
     return (
-      <Container>
+      <Container textAlign={'center'}>
+        <Card.Group>
         <Card>
           <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
           <Card.Content>
@@ -25,8 +25,47 @@ const CityPage = (props) => {
           </Card.Content>
         </Card>
 
-        <NeighbourhoodsContainer city={city}/>
+        <Card>
+          <Card.Content>
+            <Card.Header>Neighbourhoods</Card.Header>
+          </Card.Content>
+          <Card.Content>
+            <List divided relaxed>
+              {city.neighbourhoods.map(neighbourhood =>
+                <List.Item>
+                  <List bulleted horizontal link>
+                    <List.Item>{neighbourhood.name}</List.Item>
+                    <List.Item as={Link} to={`/neighbourhoods/${neighbourhood.id}`}>See details</List.Item>
+                  </List>
+                </List.Item>
+              )}
+            </List>
 
+          </Card.Content>
+        </Card>
+
+        <Card>
+          <Card.Content>
+            <Card.Header>Bicycles</Card.Header>
+          </Card.Content>
+          <Card.Content>
+            <List divided relaxed>
+              {city.bicycles.map(bicycle =>
+                <List.Item>
+                  <List.Content>
+                    <Card.Description>{bicycle.title}</Card.Description>
+                    <List bulleted horizontal link>
+                      <List.Item><Label><Icon name='tag' /> £{bicycle.price}</Label></List.Item>
+                      <List.Item as={Link} to={`/bicycles/${bicycle.id}`}>See details</List.Item>
+                    </List>
+                  </List.Content>
+                </List.Item>
+              )}
+            </List>
+          </Card.Content>
+        </Card>
+
+        </Card.Group>
       </Container>
     )
   } else {
