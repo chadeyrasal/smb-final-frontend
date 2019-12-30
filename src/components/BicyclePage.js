9aace8e0fd699bb7dd3e3a84aab3712b28d08dad
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-30 19:02:39 +0000

    build basic structure of bicycle page

--- src/components/BicyclePage.js
+++ src/components/BicyclePage.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Container, Card } from 'semantic-ui-react';
 
 const BicyclePage = (props) => {
-  return (
-    <div>BicyclePage</div>
-  )
+  let bicycle = props.bicycles.filter(bicycle => bicycle.id === parseInt(props.match.params.id))[0]
+
+  if (bicycle) {
+    return (
+      <div>
+        <Container>
+          <Card.Group>
+            <Card>
+              <Card.Content>
+                <Card.Header>{bicycle.title}</Card.Header>
+              </Card.Content>
+            </Card>
+          </Card.Group>
+        </Container>
+      </div>
+    )
+  } else {
+    return null
+  }
 }
 
 export default BicyclePage;
