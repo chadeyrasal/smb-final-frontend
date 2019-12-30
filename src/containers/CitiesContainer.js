1bfd5d896373edb9fcb3b6ec39b635cf9ea13ec8
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-30 14:38:06 +0000

    put city page component back in

--- src/containers/CitiesContainer.js
+++ src/containers/CitiesContainer.js
@@ -14,6 +14,7 @@ class CitiesContainer extends React.Component {
     return (
       <div>
         <Route exact path='/cities' render={() => <CitiesList cities={this.props.cities} />} />
+        <Route path='/cities/:id' render={(routerProps) => <CityPage {...routerProps} cities={this.props.cities} />}/>
       </div>
     )
   }
