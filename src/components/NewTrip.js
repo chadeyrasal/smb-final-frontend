e4e619548c6249aca2c06c002ba6d021f2e93d1f
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-31 15:21:08 +0000

    start work on react-dates date range picker

--- src/components/NewTrip.js
+++ src/components/NewTrip.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import momentPropTypes from 'react-moment-proptypes';
+import moment from 'moment';
+import omit from 'lodash/omit';
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
 class NewTrip extends React.Component {
+  state = {
+    startDate: null,
+    endDate: null,
+    focusedInput: null
+  };
+
   render() {
     return (
-      <div>New Trip</div>
+      <DateRangePicker
+        startDate={this.state.startDate}
+        startDateId='startDate'
+        endDate={this.state.endDate}
+        endDateId='endDate'
+        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
+        focusedInput={this.state.focusedInput}
+        onFocusChange={({ focusedInput }) => this.setState({ focusedInput })}
+      />
     )
-  }
+  };
 }
 
 export default NewTrip;
