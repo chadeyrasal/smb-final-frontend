b092b3e993f0541f1c37ee9318a31aee2edb5ea4
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-31 10:20:32 +0000

    make the Card.Group into a Grid so that each column adapts its length according to content

--- src/components/CityPage.js
+++ src/components/CityPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Card, Image, Segment, Button, List, Label, Icon } from 'semantic-ui-react';
+import { Container, Card, Image, Segment, Button, List, Label, Icon, Grid } from 'semantic-ui-react';
 
 const CityPage = (props) => {
   let city = props.cities.filter(city => city.id === parseInt(props.match.params.id))[0]
@@ -8,62 +8,74 @@ const CityPage = (props) => {
   if (city) {
     return (
       <Container textAlign={'center'}>
-        <Card.Group>
-          <Card>
-            <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
-            <Card.Content>
-              <Card.Header>{city.name}</Card.Header>
-              <Card.Meta>{city.country.name}</Card.Meta>
-            </Card.Content>
-            <Card.Content extra>
-              <Segment basic textAlign={'center'}>
-                <Button.Group vertical labeled icon basic>
-                  <Button as={Link} to={`/cities/${city.id}/bicycles`} icon='bicycle' content={`${city.bicycles.length} bicycle(s) in ${city.name}`}  />
-                  <Button as={Link} to={`/cities/${city.id}/neighbourhoods`} icon='location arrow' content={`${city.neighbourhoods.length} neighbourhood(s) in ${city.name}`} />
-                </Button.Group>
+        <Grid columns={3} divided>
+          <Grid.Row stretched>
+            <Grid.Column>
+              <Segment basic>
+                <Card>
+                  <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+                  <Card.Content>
+                    <Card.Header>{city.name}</Card.Header>
+                    <Card.Meta>{city.country.name}</Card.Meta>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <Segment basic textAlign={'center'}>
+                      <Button.Group vertical labeled icon basic>
+                        <Button as={Link} to={`/cities/${city.id}/bicycles`} icon='bicycle' content={`${city.bicycles.length} bicycle(s) in ${city.name}`}  />
+                        <Button as={Link} to={`/cities/${city.id}/neighbourhoods`} icon='location arrow' content={`${city.neighbourhoods.length} neighbourhood(s) in ${city.name}`} />
+                      </Button.Group>
+                    </Segment>
+                  </Card.Content>
+                </Card>
               </Segment>
-            </Card.Content>
-          </Card>
-
-          <Card>
-            <Card.Content>
-              <Card.Header>Neighbourhoods</Card.Header>
-            </Card.Content>
-            <Card.Content>
-              <List divided relaxed>
-                {city.neighbourhoods.map(neighbourhood =>
-                  <List.Item>
-                    <List bulleted horizontal link>
-                      <List.Item>{neighbourhood.name}</List.Item>
-                      <List.Item as={Link} to={`/neighbourhoods/${neighbourhood.id}`}>See details</List.Item>
+            </Grid.Column>
+            <Grid.Column>
+              <Segment basic>
+                <Card>
+                  <Card.Content>
+                    <Card.Header>Neighbourhoods</Card.Header>
+                  </Card.Content>
+                  <Card.Content>
+                    <List divided relaxed>
+                      {city.neighbourhoods.map(neighbourhood =>
+                        <List.Item>
+                          <List bulleted horizontal link>
+                            <List.Item>{neighbourhood.name}</List.Item>
+                            <List.Item as={Link} to={`/neighbourhoods/${neighbourhood.id}`}>See details</List.Item>
+                          </List>
+                        </List.Item>
+                      )}
                     </List>
-                  </List.Item>
-                )}
-              </List>
-            </Card.Content>
-          </Card>
-
-          <Card>
-            <Card.Content>
-              <Card.Header>Bicycles</Card.Header>
-            </Card.Content>
-            <Card.Content>
-              <List divided relaxed>
-                {city.bicycles.map(bicycle =>
-                  <List.Item>
-                    <List.Content>
-                      <Card.Description>{bicycle.title}</Card.Description>
-                      <List bulleted horizontal link>
-                        <List.Item><Label><Icon name='tag' /> £{bicycle.price}</Label></List.Item>
-                        <List.Item as={Link} to={`/bicycles/${bicycle.id}`}>See details</List.Item>
-                      </List>
-                    </List.Content>
-                  </List.Item>
-                )}
-              </List>
-            </Card.Content>
-          </Card>
-        </Card.Group>
+                  </Card.Content>
+                </Card>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <Segment basic>
+                <Card>
+                  <Card.Content>
+                    <Card.Header>Bicycles</Card.Header>
+                  </Card.Content>
+                  <Card.Content>
+                    <List divided relaxed>
+                      {city.bicycles.map(bicycle =>
+                        <List.Item>
+                          <List.Content>
+                            <Card.Description>{bicycle.title}</Card.Description>
+                            <List bulleted horizontal link>
+                              <List.Item><Label><Icon name='tag' /> £{bicycle.price}</Label></List.Item>
+                              <List.Item as={Link} to={`/bicycles/${bicycle.id}`}>See details</List.Item>
+                            </List>
+                          </List.Content>
+                        </List.Item>
+                      )}
+                    </List>
+                  </Card.Content>
+                </Card>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Container>
     )
   } else {
