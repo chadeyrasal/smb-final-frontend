164deea96d0dbe4c960b93dcb73b9d5a4039684b
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-31 16:21:27 +0000

    add picture to homepage

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import BicycleImage from '../images/black-bicycle.jpg';
+import { Image } from 'semantic-ui-react';
 
 class HomeContainer extends React.Component {
   render() {
     return (
-      <div>HomeContainer</div>
+      <div>
+        <Image src={BicycleImage} fluid />
+      </div>
     )
   }
 }
