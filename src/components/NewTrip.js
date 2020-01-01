a1c8f383661a9a93977dad268fe510d31ed17441
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-01 15:11:17 +0000

    build new trip form properly

--- src/components/NewTrip.js
+++ src/components/NewTrip.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import 'react-dates/initialize';
-import { DateRangePicker } from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
-import { Container, Form } from 'semantic-ui-react';
+import { Container, Form, Segment, Button, Grid } from 'semantic-ui-react';
 
 class NewTrip extends React.Component {
   state = {
-    startDate: null,
-    endDate: null,
-    focused: null
-  };
+    startDate: new Date(),
+    endDate: new Date()
+  }
 
   render() {
     return (
       <div>
-        New Trip
+        <Container>
+          <Grid columns={2}>
+            <Grid.Row stretched>
+              <Grid.Column>
+                <Segment basic></Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment basic>
+                  <Form>
+                    <Form.Field>
+                      <label>Start Date: </label>
+                      <input type='date' value={this.state.startDate} name='startDate' onChange={event => this.setState({startDate: event.target.value})} />
+                    </Form.Field>
+                    <Form.Field>
+                      <label>End Date: </label>
+                      <input type='date' value={this.state.endDate} name='endDate' onChange={event => this.setState({endDate: event.target.value})} />
+                    </Form.Field>
+                    <Segment basic textAlign={'center'}>
+                      <Button type='submit' color='olive'>Book this bicycle</Button>
+                    </Segment>
+                  </Form>
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
       </div>
     )
   };
