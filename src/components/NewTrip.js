dee4324bad18c75c90ba02663fc35b36fabc35dc
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-01 15:40:17 +0000

    remove the container and grid formatting from the component and add the renter and bicycle to the local state

--- src/components/NewTrip.js
+++ src/components/NewTrip.js
@@ -1,41 +1,34 @@
 import React from 'react';
-import { Container, Form, Segment, Button, Grid } from 'semantic-ui-react';
+import { Form, Segment, Button } from 'semantic-ui-react';
 
 class NewTrip extends React.Component {
   state = {
     startDate: new Date(),
-    endDate: new Date()
+    endDate: new Date(),
+    bicycle: this.props.bicycle,
+    renter: this.props.currentUser
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
   }
 
   render() {
     return (
       <div>
-        <Container>
-          <Grid columns={2}>
-            <Grid.Row stretched>
-              <Grid.Column>
-                <Segment basic></Segment>
-              </Grid.Column>
-              <Grid.Column>
-                <Segment basic>
-                  <Form>
-                    <Form.Field>
-                      <label>Start Date: </label>
-                      <input type='date' value={this.state.startDate} name='startDate' onChange={event => this.setState({startDate: event.target.value})} />
-                    </Form.Field>
-                    <Form.Field>
-                      <label>End Date: </label>
-                      <input type='date' value={this.state.endDate} name='endDate' onChange={event => this.setState({endDate: event.target.value})} />
-                    </Form.Field>
-                    <Segment basic textAlign={'center'}>
-                      <Button type='submit' color='olive'>Book this bicycle</Button>
-                    </Segment>
-                  </Form>
-                </Segment>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field>
+            <label>Start Date: </label>
+            <input type='date' value={this.state.startDate} name='startDate' onChange={event => this.setState({startDate: event.target.value})} />
+          </Form.Field>
+          <Form.Field>
+            <label>End Date: </label>
+            <input type='date' value={this.state.endDate} name='endDate' onChange={event => this.setState({endDate: event.target.value})} />
+          </Form.Field>
+          <Segment basic textAlign={'center'}>
+            <Button type='submit' color='olive'>Book this bicycle</Button>
+          </Segment>
+        </Form>
       </div>
     )
   };
