9a7c3f2234bc3f1814d47ce8e44d1458cb3cb4fa
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-01 16:50:04 +0000

    build the neighbourhoods list component to display neighbourhoods properly and set up a link to the neighbourhoods bicycles

--- src/components/NeighbourhoodsList.js
+++ src/components/NeighbourhoodsList.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Card, Image, Segment, Button0 } from 'semantic-ui-react';
 
 const NeighbourhoodsList = (props) => {
-  return (
-    <div>
-      NeighbourhoodsList
-    </div>
-  )
+  let city = props.cities.filter(city => city.id === parseInt(props.match.params.id))[0]
+
+  if (city) {
+    return (
+      <div>
+        <Container>
+          <Card.Group>
+            {city.neighbourhoods.map(neighbourhood =>
+              <Card key={neighbourhood.id}>
+                <Card.Content>
+                  <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/molly.png'/>
+                  <Card.Header>{neighbourhood.name}</Card.Header>
+                  <Card.Meta><Link to={`/cities/${city.id}`}>{city.name}</Link></Card.Meta>
+                </Card.Content>
+                <Card.Content extra>
+                  <Segment basic textAlign={'center'}>
+                    <Button.Group vertical labeled icon basic>
+                      <Button as={Link} to={`/neighbourhoods/${neighbourhood.id}/bicycles`} icon='bicycle' content='Sees all Bicycles' />
+                    </Button.Group>
+                  </Segment>
+                </Card.Content>
+              </Card>
+            )}
+          </Card.Group>
+        </Container>
+      </div>
+    )
+  } else {
+    return null
+  }
 };
 
 export default NeighbourhoodsList;
