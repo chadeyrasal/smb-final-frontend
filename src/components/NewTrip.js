e6011b0b0db71ee673661711db6eb4d0f1bd6bbe
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-01 13:45:21 +0000

    add all necessary import for new trip form and react-dates

--- src/components/NewTrip.js
+++ src/components/NewTrip.js
@@ -1,29 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import momentPropTypes from 'react-moment-proptypes';
-import moment from 'moment';
-import omit from 'lodash/omit';
+import 'react-dates/initialize';
 import { DateRangePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
+import { Container, Form } from 'semantic-ui-react';
 
 class NewTrip extends React.Component {
   state = {
     startDate: null,
     endDate: null,
-    focusedInput: null
+    focused: null
   };
 
   render() {
     return (
-      <DateRangePicker
-        startDate={this.state.startDate}
-        startDateId='startDate'
-        endDate={this.state.endDate}
-        endDateId='endDate'
-        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
-        focusedInput={this.state.focusedInput}
-        onFocusChange={({ focusedInput }) => this.setState({ focusedInput })}
-      />
+      <div>
+        New Trip
+      </div>
     )
   };
 }
