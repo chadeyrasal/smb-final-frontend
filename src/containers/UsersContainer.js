93fc228b79a283b09228043cf7661c0512a137df
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-01 18:00:47 +0000

    build users container so that it renders the appropriate components for the user page and for theuser profile

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
+import UserPage from '../components/UserPage';
+import UserProfile from '../components/UserProfile';
+import { fetchUsers } from '../actions/users';
 
 class UsersContainer extends React.Component {
+  componentDidMount() {
+    this.props.fetchUsers()
+  }
+
   render() {
     return (
       <div>
-        UsersContainer
+        <Route exact path='/users/:id' render={() => <UserPage />} />
+        <Route path='/users/:id/profile' component={UserProfile} />
       </div>
     )
   }
 }
 
-export default UsersContainer;
+const mapStateToProps = state => {
+  return {
+    users: state.users
+  }
+}
+
+export default connect(mapStateToProps, { fetchUsers })(UsersContainer);
