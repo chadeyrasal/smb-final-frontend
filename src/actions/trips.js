6b9bcffee3a96991329b1f479b3da5ec1787037e
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-01 16:01:14 +0000

    create action creator file for trips and build the addTrip action creator with fetch

---
+++ src/actions/trips.js
@@ -0,0 +1,17 @@
+export const addTrip = (formData) => {
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/trips', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(trip => dispatch({
+        type: 'ADD_TRIP',
+        payload: trip
+      }))
+  }
+}
