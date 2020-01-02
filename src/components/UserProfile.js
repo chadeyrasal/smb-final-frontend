152a518b810ccb6775f9763d6a978e3cfdddc69e
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-02 14:58:40 +0000

    build basic structure for profile component and check the correct data is displayed

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Container, Grid, Segment, Card } from 'semantic-ui-react';
 
 const UserProfile = (props) => {
-  return (
-    <div>User Profile to see any user</div>
-  )
+  let user = props.users.filter(user => user.id === parseInt(props.match.params.id))[0]
+
+  if (user) {
+    return (
+      <Container>
+        <Grid columns={3} divided>
+          <Grid.Row stretched>
+            <Grid.Column>
+              <Segment basic>
+                {user.fullname}
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    )
+  } else {
+    return null
+  }
 }
 
 export default UserProfile;
