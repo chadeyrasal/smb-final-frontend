256af5c1629840d6f2833f7b9a6fba97a5e37dc6
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-02 18:00:51 +0000

    add picture, fullname and membership date to first column ofgrid inside card component

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Grid, Segment } from 'semantic-ui-react';
+import { Container, Grid, Segment, Image, Card } from 'semantic-ui-react';
 
 const UserProfile = (props) => {
   let user = props.users.filter(user => user.id === parseInt(props.match.params.id))[0]
@@ -13,7 +13,13 @@ const UserProfile = (props) => {
           <Grid.Row stretched>
             <Grid.Column>
               <Segment basic>
-                {user.fullname}
+                <Card>
+                  <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
+                  <Card.Content>
+                    <Card.Header>{user.fullname}</Card.Header>
+                    <Card.Meta>Member since: {user.created_at}</Card.Meta>
+                  </Card.Content>
+                </Card>
               </Segment>
             </Grid.Column>
             <Grid.Column>
