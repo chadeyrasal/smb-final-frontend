dbe312f703d7371ec9bcb822e3d437369ddcebef
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-03 17:44:56 +0000

    add list of bicycles owned by the user, with links to each bicycle page

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Grid, Segment, Image, Card, Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Container, Grid, Segment, Image, Card, Header, List, Label, Icon } from 'semantic-ui-react';
 
 const UserProfile = (props) => {
   let user = props.users.filter(user => user.id === parseInt(props.match.params.id))[0]
@@ -26,13 +27,28 @@ const UserProfile = (props) => {
               <Segment basic textAlign={'center'}>
                 <Header>About {user.fullname}</Header>
                 {user.description ? <Segment basic>{user.description}</Segment> : <Segment basic>There is no about me for {user.fullname} yet</Segment>}
-
               </Segment>
             </Grid.Column>
             <Grid.Column>
               <Segment basic textAlign={'center'}>
-                <Header>{user.fullname}'s bicycles </Header>
-                {user.fullname}
+                <Header>Bicycles </Header>
+                <Card>
+                  <Card.Content>
+                    <List divided relaxed>
+                      {user.bicycles.map(bicycle =>
+                        <List.Item key={bicycle.id}>
+                          <List.Content>
+                            <Card.Description>{bicycle.title}</Card.Description>
+                            <List bulleted horizontal link>
+                              <List.Item><Label><Icon name='tag' /> £{bicycle.price}</Label></List.Item>
+                              <List.Item as={Link} to={`/bicycles/${bicycle.id}`}>See details</List.Item>
+                            </List>
+                          </List.Content>
+                        </List.Item>
+                      )}
+                    </List>
+                  </Card.Content>
+                </Card>
               </Segment>
             </Grid.Column>
           </Grid.Row>
