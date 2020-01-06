dfbd6a522350abcedb55c2620c3545c76823f351
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-06 20:00:45 +0000

    add space between end of page and beginning of footer

--- src/components/FooterLoggedOut.js
+++ src/components/FooterLoggedOut.js
@@ -6,6 +6,7 @@ class FooterLoggedOut extends React.Component {
   render() {
     return (
       <div>
+        <Segment basic></Segment>
         <Segment attached='bottom' textAlign={'center'} secondary>
           <p><Link className='footer-para' to='/about'>About Share My Bike</Link></p>
           <p>{'\u00a9'} Share My Bike 2020. All rights reserved</p>
