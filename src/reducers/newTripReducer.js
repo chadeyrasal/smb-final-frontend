2de378a742aff8ed3d96109519fd3d76fadc5a60
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-10 20:24:47 +0000

    add new case for reset new trip form and rename add trip case into create trip

--- src/reducers/newTripReducer.js
+++ src/reducers/newTripReducer.js
@@ -9,7 +9,9 @@ export default function newTripReducer(state = initialState, action) {
   switch (action.type) {
     case 'UPDATE_NEW_TRIP_FORM':
       return action.payload
-    case 'ADD_TRIP':
+    case 'RESET_NEW_TRIP_FORM':
+      return initialState
+    case 'CREATE_TRIP':
       return [...state, action.payload]
     default:
       return state
