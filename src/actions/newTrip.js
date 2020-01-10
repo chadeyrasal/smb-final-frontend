ce1a1cb7dd388a8163a3d27b2653812223e88be6
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-10 20:24:04 +0000

    reshape addTrip action creator, build createTrip and resetNewTripForm action creators, dispatch the newly created actions creators in the addTrip asynchronous creator

--- src/actions/newTrip.js
+++ src/actions/newTrip.js
@@ -1,25 +1,43 @@
-export const updateNewTripForm = (formData) => {
+export const updateNewTripForm = formData => {
   return {
     type: 'UPDATE_NEW_TRIP_FORM',
     payload: formData
   }
 }
 
-export const addTrip = (formData) => {
+export const createTrip = trip => {
+  return {
+    type: 'CREATE_TRIP',
+    payload: trip
+  }
+}
+
+export const resetNewTripForm = () => {
+  return {
+    type: 'RESET_NEW_TRIP_FORM'
+  }
+}
+
+export const addTrip = formData => {
   return (dispatch) => {
     fetch('http://localhost:3000/api/v1/trips', {
       credentials: 'include',
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       },
-      method: 'POST',
       body: JSON.stringify(formData)
     })
       .then(response => response.json())
-      .then(trip => dispatch({
-        type: 'ADD_TRIP',
-        payload: trip
-      }))
+      .then(trip => {
+        if (trip.error) {
+          alert(trip.error)
+        } else {
+          dispatch(createTrip(trip))
+          dispatch(resetNewTripForm())
+        }
+      })
+      .catch(console.log)
   }
 }
