8989002af9cbcd3ea80861491af659a9ad0b78bc
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-12 19:57:32 +0000

    remove addProps method and push info on bicycle and renter in the handle change method

--- src/components/NewTrip.js
+++ src/components/NewTrip.js
@@ -3,28 +3,20 @@ import { connect } from 'react-redux';
 import { updateNewTripForm, addTrip } from '../actions/newTrip';
 import { Form, Segment, Button } from 'semantic-ui-react';
 
-const NewTrip = ({newTripForm, bicycle, currentUser, updateNewTripForm}) => {
-  const addProps = () => {
-    const addedProps = {
-      ...newTripForm,
-      bicycle: bicycle,
-      renter: currentUser
-    };
-    updateNewTripForm(addedProps);
-  }
-
+const NewTrip = ({newTripForm, bicycle, currentUser, updateNewTripForm, addTrip}) => {
   const handleChange = event => {
     const {name, value} = event.target;
     const updatedFormInfo = {
       ...newTripForm,
-      [name]: value
+      [name]: value,
+      bicycle: bicycle,
+      renter: currentUser
     };
     updateNewTripForm(updatedFormInfo);
   }
 
   const handleSubmit = event => {
     event.preventDefault();
-    addProps();
     addTrip(newTripForm);
   }
 
@@ -46,40 +38,6 @@ const NewTrip = ({newTripForm, bicycle, currentUser, updateNewTripForm}) => {
     </div>
   )
 }
-
-// class NewTrip extends React.Component {
-//   state = {
-//     startDate: new Date(),
-//     endDate: new Date(),
-//     bicycle: this.props.bicycle,
-//     renter: this.props.currentUser
-//   }
-//
-//   handleSubmit(event) {
-//     event.preventDefault()
-//     debugger
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         <Form onSubmit={this.handleSubmit}>
-//           <Form.Field>
-//             <label>Start Date: </label>
-//             <input type='date' value={this.state.startDate} name='startDate' onChange={event => this.setState({startDate: event.target.value})} />
-//           </Form.Field>
-//           <Form.Field>
-//             <label>End Date: </label>
-//             <input type='date' value={this.state.endDate} name='endDate' onChange={event => this.setState({endDate: event.target.value})} />
-//           </Form.Field>
-//           <Segment basic textAlign={'center'}>
-//             <Button type='submit' color='olive'>Book this bicycle</Button>
-//           </Segment>
-//         </Form>
-//       </div>
-//     )
-//   }
-// }
 
 const mapStateToProps = state => {
   return {
