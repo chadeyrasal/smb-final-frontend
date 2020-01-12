70565d931660045d36f43acd76753f265d6e5168
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-12 12:31:44 +0000

    remove create_trip from current user reducer

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -4,8 +4,6 @@ export default function currentUserReducer(state = null, action) {
       return action.payload
     case 'CLEAR_CURRENT_USER':
       return null
-    case 'CREATE_TRIP':
-      return {...state, state.trips: [...state.trips, action.payload]}
     default:
       return state
   }
