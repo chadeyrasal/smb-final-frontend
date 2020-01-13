591a490304d2156c32bf2412e714637a23c4a96b
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-13 16:18:25 +0000

    add styling to create bicycle form

--- src/components/CreateBicycleForm.js
+++ src/components/CreateBicycleForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {addBicycle} from '../actions/addBicycle'
+import { connect } from 'react-redux';
+import { addBicycle } from '../actions/addBicycle';
+import { Form, Segment, Button, Header, Container } from 'semantic-ui-react';
 
 class CreateBicycleForm extends React.Component {
   state = {
@@ -33,51 +34,65 @@ class CreateBicycleForm extends React.Component {
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <h2>New Listing</h2><br/>
-
-          <label>Listing Title: </label><br/>
-          <input type='text' value={this.state.title} name='title' onChange={this.handleChange}></input><br/><br/>
-
-          <label>Listing Description: </label><br/>
-          <textarea value={this.state.description} name='description' onChange={this.handleChange}></textarea><br/><br/>
-
-          <label>Bicycle Type: </label><br/>
-          <select name='bicycle_type' value={this.state.bicycle_type} onChange={this.handleChange}>
-            <option></option>
-            <option>City Bicycle</option>
-            <option>Mountain Bicycle</option>
-            <option>Road Bicycle</option>
-            <option>Folding Bicycle</option>
-            <option>Trekking Bicycle</option>
-          </select><br/><br/>
-
-          <label>Bicycle Size: </label><br/>
-          <select name='size' value={this.state.size} onChange={this.handleChange}>
-            <option></option>
-            <option>Small</option>
-            <option>Medium</option>
-            <option>Large</option>
-          </select><br/><br/>
-
-          <label>Bicycle Colour: </label><br/>
-          <input type='text' value={this.state.colour} name='colour' onChange={this.handleChange}></input><br/><br/>
-
-          <label>Price: </label><br/>
-          <input type='text' value={this.state.price} name='price' onChange={this.handleChange}></input><br/><br/>
-
-          <label>Country: </label><br/>
-          <select></select><br/><br/>
-
-          <label>City: </label><br/>
-          <select></select><br/><br/>
-
-          <label>Neighbourhood: </label><br/>
-          <select></select><br/><br/>
-
-          <input type='submit' value='Publish Listing'></input>
-        </form>
+      <div className='form-wrapper'>
+        <Container>
+          <Segment basic textAlign={'center'}>
+            <Header>New Bicycle Listing</Header>
+          </Segment>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Field>
+              <label>Listing Title: </label>
+              <input type='text' value={this.state.title} name='title' onChange={this.handleChange}></input>
+            </Form.Field>
+            <Form.Field>
+              <label>Listing Description: </label>
+              <textarea value={this.state.description} name='description' onChange={this.handleChange}></textarea>
+            </Form.Field>
+            <Form.Field>
+              <label>Bicycle Type: </label>
+              <select name='bicycle_type' value={this.state.bicycle_type} onChange={this.handleChange}>
+                <option></option>
+                <option>City Bicycle</option>
+                <option>Mountain Bicycle</option>
+                <option>Road Bicycle</option>
+                <option>Folding Bicycle</option>
+                <option>Trekking Bicycle</option>
+              </select>
+            </Form.Field>
+            <Form.Field>
+              <label>Bicycle Size: </label>
+              <select name='size' value={this.state.size} onChange={this.handleChange}>
+                <option></option>
+                <option>Small</option>
+                <option>Medium</option>
+                <option>Large</option>
+              </select>
+            </Form.Field>
+            <Form.Field>
+              <label>Bicycle Colour: </label>
+              <input type='text' value={this.state.colour} name='colour' onChange={this.handleChange}></input>
+            </Form.Field>
+            <Form.Field>
+              <label>Price: </label>
+              <input type='text' value={this.state.price} name='price' onChange={this.handleChange}></input>
+            </Form.Field>
+            <Form.Field>
+              <label>Country: </label>
+              <select></select>
+            </Form.Field>
+            <Form.Field>
+              <label>City: </label>
+              <select></select>
+            </Form.Field>
+            <Form.Field>
+              <label>Neighbourhood: </label>
+              <select></select>
+            </Form.Field>
+            <Segment basic textAlign={'center'}>
+              <Button type='submit' color='olive'>Publish Listing</Button>
+            </Segment>
+          </Form>
+        </Container>
       </div>
     )
   }
