3b427acf8fa62380899cf950b3100232794f1e05
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-14 21:37:17 +0000

    start work on bicycle creation review

--- src/actions/addBicycle.js
+++ src/actions/addBicycle.js
@@ -1,17 +1,18 @@
 export const addBicycle = (formData) => {
+  console.log(formData)
   return (dispatch) => {
-    fetch('http://localhost:3000/api/v1/bicycles', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      method: 'POST',
-      body: JSON.stringify(formData) // this way formData is treatedas a string, when initially it is an object
-    })
-      .then(response => response.json())
-      .then(bicycle => dispatch({
-        type: 'ADD_BICYCLE',
-        payload: bicycle
-      }))
+    // fetch('http://localhost:3000/api/v1/bicycles', {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Accept': 'application/json'
+    //   },
+    //   method: 'POST',
+    //   body: JSON.stringify(formData) // this way formData is treatedas a string, when initially it is an object
+    // })
+    //   .then(response => response.json())
+    //   .then(bicycle => dispatch({
+    //     type: 'ADD_BICYCLE',
+    //     payload: bicycle
+    //   }))
   }
 }
