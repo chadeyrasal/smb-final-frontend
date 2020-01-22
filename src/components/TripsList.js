e55236a1649431990623c26363cb119b127b896f
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-22 12:00:30 +0000

    add header to the page and ensure proper data is sent

--- src/components/TripsList.js
+++ src/components/TripsList.js
@@ -1,1 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Container, Header, Segment } from 'semantic-ui-react';
+
+const TripsList = ({ currentUser }) => {
+  if (currentUser) {
+    return (
+      <Container>
+        <Segment basic textAlign={'center'}>
+          <Header>Your Trips</Header>
+        </Segment>
+      </Container>
+    )
+  } else {
+    return null
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    currentUser: state.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(TripsList);
