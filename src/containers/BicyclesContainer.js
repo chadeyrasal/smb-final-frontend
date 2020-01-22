571841e581c5b836cee61b0f3625cd399500a2c8
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-22 18:11:18 +0000

    add fetchCountries to bicycles container in component did mount

--- src/containers/BicyclesContainer.js
+++ src/containers/BicyclesContainer.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 import { fetchBicycles } from '../actions/fetchBicycles';
+import { fetchCountries } from '../actions/fetchCountries';
 import BicyclePage from '../components/BicyclePage';
 import CreateBicycleForm from '../components/CreateBicycleForm';
 
 class BicyclesContainer extends React.Component {
   componentDidMount() {
     this.props.fetchBicycles()
+    this.props.fetchCountries()
   }
 
   render() {
     return (
       <div>
         <Route path='/bicycles/:id' render={(routerProps) => <BicyclePage {...routerProps} bicycles={this.props.bicycles} />} />
-        <Route exact path='/bicycles/new' component={CreateBicycleForm} />
+        <Route exact path='/bicycles/new' render={() => <CreateBicycleForm countries={this.props.countries}/>} />
       </div>
     )
   }
@@ -26,4 +28,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, {fetchBicycles})(BicyclesContainer);
+export default connect(mapStateToProps, { fetchBicycles, fetchCountries })(BicyclesContainer);
