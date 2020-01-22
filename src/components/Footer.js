e4e273c318a67bf3072885340fe5556c538c27eb
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-22 17:48:50 +0000

    rename FooterLoggedIn into Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import LogOut from './Logout';
+import { Segment, Divider } from 'semantic-ui-react';
+
+const Footer = ({currentUser}) => {
+  return (
+    <div>
+      <Segment basic></Segment>
+      <Segment attached='bottom' textAlign={'center'} secondary>
+        {currentUser ? <div><h5>You are logged in as <Link className='footer-para' to={`/users/${currentUser.id}`}>{currentUser.fullname}</Link> {'\u00b7'} <LogOut /></h5><Divider /></div> : null}
+        <p><Link className='footer-para' to='/about'>About Share My Bike</Link></p>
+        <p>{'\u00a9'} Share My Bike 2020. All rights reserved</p>
+      </Segment>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Footer);
