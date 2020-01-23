0a7139e300ba20dc492c30548a0c1c40731a7f24
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-23 16:17:00 +0000

    change visual of trips list for user page

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -45,11 +45,9 @@ const UserPage = (props) => {
                       {user.reservations.map(resa =>
                         <List.Item key={resa.id}>
                           <List.Content>
-                            {console.log(resa)}
-                            <Card.Description>{resa.bicycle.title}</Card.Description>
+                            <Card.Description><Link to={`/bicycles/${resa.bicycle.id}`}>{resa.bicycle.title}</Link></Card.Description>
                             <Card.Meta>{new Intl.DateTimeFormat('en-GB', {month: 'short', day: 'numeric'}).format(new Date(Date.parse(resa.start_date)))} to {new Intl.DateTimeFormat('en-GB', {year: 'numeric', month: 'short', day: 'numeric'}).format(new Date(Date.parse(resa.end_date)))}</Card.Meta>
-                            <Divider />
-                            {resa.review ? <Card.Meta>resa.review</Card.Meta> : <Button as={Link} to={`/trips/${resa.id}/edit`}>Review Your Trip</Button>}
+                            {resa.review ? <Card.Meta>resa.review</Card.Meta> : <Button basic as={Link} to={`/trips/${resa.id}/edit`}>Review Your Trip</Button>}
                           </List.Content>
                         </List.Item>
                       )}
