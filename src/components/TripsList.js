a642e85228c15425d742002bab6f9ce2ee25038b
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-23 16:17:45 +0000

    add appropriate information and styling to trips index

--- src/components/TripsList.js
+++ src/components/TripsList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Container, Header, Segment, Button } from 'semantic-ui-react';
+import { Container, Header, Segment, Button, Card, Divider, Icon } from 'semantic-ui-react';
 
 const TripsList = ({ currentUser }) => {
   if (currentUser) {
@@ -11,11 +11,22 @@ const TripsList = ({ currentUser }) => {
           <Header>Your Trips</Header>
           {currentUser.reservations.length === 0 ? <Segment basic>You haven't booked any trip yet</Segment> : null}
           {currentUser.reservations.length === 0 ? <Button as={Link} to='/cities'>Browse Cities</Button> : null}
-          <ul>
+          <Card.Group>
             {currentUser.reservations.map(resa =>
-              <li>{resa.start_date}</li>
+              <Card key={resa.id}>
+                <Card.Content>
+                  <Card.Header>Your {new Intl.DateTimeFormat('en-GB', {month: 'long'}).format(new Date(Date.parse(resa.start_date)))} trip</Card.Header>
+                  <Card.Meta>Your {new Intl.DateTimeFormat('en-GB', {month: 'short', day: 'numeric'}).format(new Date(Date.parse(resa.start_date)))} to {new Intl.DateTimeFormat('en-GB', {year: 'numeric', month: 'short', day: 'numeric'}).format(new Date(Date.parse(resa.end_date)))} Trip</Card.Meta>
+                  <Divider horizontal>
+                    <Header as='h4'><Icon name='bicycle' />The Bicycle</Header>
+                  </Divider>
+                  <Card.Description><Link to={`/bicycles/${resa.bicycle.id}`}>{resa.bicycle.title}</Link></Card.Description>
+                  <Divider />
+                  <Card.Description><Button as={Link} to={`/users/${currentUser.id}/trips/${resa.id}/edit`} basic>Review your Trip</Button></Card.Description>
+                </Card.Content>
+              </Card>
             )}
-          </ul>
+          </Card.Group>
         </Segment>
       </Container>
     )
