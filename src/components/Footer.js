650be44fc8065b3d46516082539f11a2a2d0a69c
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-23 16:15:39 +0000

    add social media icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import LogOut from './Logout';
-import { Segment, Divider } from 'semantic-ui-react';
+import { Segment, Divider, Button } from 'semantic-ui-react';
 
 const Footer = ({currentUser}) => {
   return (
@@ -11,6 +11,9 @@ const Footer = ({currentUser}) => {
       <Segment attached='bottom' textAlign={'center'} secondary>
         {currentUser ? <div><h5>You are logged in as <Link className='footer-para' to={`/users/${currentUser.id}`}>{currentUser.fullname}</Link> {'\u00b7'} <LogOut /></h5><Divider /></div> : null}
         <p><Link className='footer-para' to='/about'>About Share My Bike</Link></p>
+        <p><Button circular color='facebook' icon='facebook' />
+        <Button circular color='twitter' icon='twitter' />
+        <Button circular color='linkedin' icon='linkedin' /></p>
         <p>{'\u00a9'} Share My Bike 2020. All rights reserved</p>
       </Segment>
     </div>
